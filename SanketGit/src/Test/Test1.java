ae9daceb274bdae4cd1b204f4b335220bc2e4268
Sanket832949 <85669078+Sanket832949@users.noreply.github.com>
2021-08-04 09:46:22 +0530

    Update Test1.java

--- SanketGit/src/Test/Test1.java
+++ SanketGit/src/Test/Test1.java
@@ -7,7 +7,7 @@ public static void main(String[] args)
 	System.out.println("Hello Sanket");
 	System.out.println("Hello Balu");
 	System.out.println("Hello Sagar");
-	System.out.println("Hello Sameer0245");
+	System.out.println("Hello Sameer0123456");
 	
 }
 
